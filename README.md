31b29d3a6954598e848677f48193d81d37de3ad9
meir@redislabs.com <meir@redislabs.com>
2019-10-27 15:40:57 +0200

    added exmaple files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ImdbExample
 RedisGears Imdb Example
+
+## Run the Example
+* Set up you redis server with RedisGears
+* run `python UploadImdb.py`
+* run `RG.PYEXECUTE "GB('KeysOnlyReader').map(lambda x: execute('hget', x, 'genres')).flatmap(lambda x: x.split(',')).countby().run()"`
---
+++ UploadImdb.py
@@ -0,0 +1,51 @@
+import argparse
+import redis
+import csv
+
+CONFIG_FILE_PREFIX = '@'
+CONFIG_FILE_NAME = 'config.txt'
+
+parser = argparse.ArgumentParser(description='Upload imdb data has hashes into redis')
+
+parser.add_argument('-H', '--host', default='localhost', help='redis host')
+
+parser.add_argument('-P', '--port', default=6379, help='redis port')
+
+parser.add_argument('-F', '--imdb-file-path', default='./data.tsv', help='path to imdb file data')
+
+parser.add_argument('--delimiter', default='\t', help='csv delimiter')
+
+parser.add_argument('--betch', default=10000, help='batch size')
+
+parser.add_argument('--num-hashes', default=30000, help='number of hashes to create')
+
+parser.add_argument('-v', '--verbose', action='count', default=0, help='print more information about the test')
+
+args = parser.parse_args()
+
+print args
+
+r = redis.Redis(args.host, args.port)
+
+with open(args.imdb_file_path) as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=args.delimiter)
+	fields = csv_reader.next()
+	numHashes = 1
+	pipe = r.pipeline(transaction=False)
+	for line in csv_reader:
+		d = {}
+		for i in range(len(line)):
+			d[fields[i]] = line[i]
+		pipe.hmset(d['tconst'], d)
+		if(args.verbose > 1):
+			print('added hash to batch: %d' % numHashes)
+		if numHashes % int(args.betch) == 0:
+			if(args.verbose > 0):
+				print('sent batch: %d' % numHashes)
+			pipe.execute()
+			pipe = r.pipeline(transaction=False)
+		if args.num_hashes != 'inf' and numHashes == int(args.num_hashes):
+			break
+		numHashes += 1
+	pipe.execute()
+	print('done')
